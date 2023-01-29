a31a28df15a5091fc2338652de72791944687ece
Anna_Dmitrieva <catdogcat000gmail.com>
2023-01-30 01:37:58 +0300

    added lesson 10 tasks

---
+++ homework_lesson10/data.js
@@ -0,0 +1,23 @@
+const dataInfo = `[
+    {
+        "id": "ft1",
+        "url": "https://cdn2.thecatapi.com/images/2o5.jpg",
+        "name": "First cat",
+        "descBtn": "Open first cat",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, enim?"
+    },
+    {
+        "id": "jh2",
+        "url": "https://cdn2.thecatapi.com/images/9r8.jpg",
+        "name": "Second cat",
+        "descBtn": "Open second cat",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, deleniti."
+    },
+    {
+        "id": "st3",
+        "url": "https://cdn2.thecatapi.com/images/c5i.png",
+        "name": "Third cat",
+        "descBtn": "Open third cat",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, dolor?"
+    }
+]`
